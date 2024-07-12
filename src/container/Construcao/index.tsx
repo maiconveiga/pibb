08a420e6979ec229e09b07bc11419f728d10c5f0
= <=>
2024-07-11 21:37:53 -0300

    feat: create a react setup using eslint, prettier and styled components

---
+++ src/container/Construcao/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from './style'
+
+const EmConstrucao = () => {
+  return (
+    <Container>
+      <div className="content">
+        <h1>Site em Construção</h1>
+        <p>
+          Estamos trabalhando para trazer algo incrível para você. Volte em
+          breve!
+        </p>
+      </div>
+    </Container>
+  )
+}
+
+export default EmConstrucao
